2d2357d484c699870a2a6d7da8e74cb7ba2d35b8
Renan <130794999+RenanNeets@users.noreply.github.com>
2024-09-20 18:23:25 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+"""
+
+# Count é um iterador sem fim
+from itertools import count
+c1 = count(start=1,step=8)
+#Vendo count com iterator
+print('c1', hasattr(c1,'__iter__')) #É um iterravel
+print('c1', hasattr(c1,'__next__')) #É um iterator
+for i in c1:
+    if i > 100:
+        break
+    print(i)
+    if i > 100:
+        break
+
+"""
+
+"""
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+from itertools import combinations, permutations, product
+
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia',
+]
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g'],
+    ['masculino', 'feminino', 'unisex'],
+    ['algodão', 'poliéster']
+]
+
+print('Combinação:')
+print_iter(combinations(pessoas, 2))
+print('Permutação:')
+print_iter(permutations(pessoas, 2))
+print('Produto:')
+print_iter(product(*camisetas))
+"""
+
+
+
+"""
+# groupby - agrupando valores (itertools)
+from itertools import groupby
+
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+]
+
+
+def ordena(aluno):
+    return aluno['nota'] #Chave que você quer ordenar
+
+alunos_agrupados = sorted(alunos, key=ordena)
+grupos = groupby(alunos_agrupados, key=ordena)
+
+for chave, grupo in grupos:
+    print(chave)
+    for aluno in grupo:
+        print(aluno)
+"""
+
